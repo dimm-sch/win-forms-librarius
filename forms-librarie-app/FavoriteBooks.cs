5260877796baa7f40ab624a79755debc8471af65
dimm-sch <dumitruceiti@gmail.com>
2022-05-19 23:06:44 +0300

    feat: add date and librarius header to exported excel file

--- forms-librarie-app/FavoriteBooks.cs
+++ forms-librarie-app/FavoriteBooks.cs
@@ -43,17 +43,22 @@ namespace forms_librarie_app
 			WorkBook workBook = WorkBook.Create(ExcelFileFormat.XLSX);
 			var sheet = workBook.CreateWorkSheet("Cărți favorite");
 
-			//sheet[""].Value = "";
-			sheet["B3"].Value = "Carte";
-			sheet["C3"].Value = "Autor";
-			sheet["D3"].Value = "Editura";
-			sheet["E3"].Value = "Anul";
-			sheet["F3"].Value = "Pagini";
-			sheet["G3"].Value = "Gen";
-			sheet["H3"].Value = "ISBN";
-			sheet["I3"].Value = "Preț";
-			// Venus ia foc Marte e de gheata
-			string tableNameRange = "B2:I2";
+			string librarius = "D2:E2";
+			sheet[librarius].Value = "Librarius";
+			sheet[librarius].Style.Font.Bold = true;
+			sheet[librarius].Style.Font.Height = 20;
+			sheet.Merge(librarius);
+
+			sheet["B5"].Value = "Carte";
+			sheet["C5"].Value = "Autor";
+			sheet["D5"].Value = "Editura";
+			sheet["E5"].Value = "Anul";
+			sheet["F5"].Value = "Pagini";
+			sheet["G5"].Value = "Gen";
+			sheet["H5"].Value = "ISBN";
+			sheet["I5"].Value = "Preț";
+			
+			string tableNameRange = "B4:I4";
 			var tableName = sheet[tableNameRange];
 			tableName.Value = "Cărți favorite";
 			tableName.Style.HorizontalAlignment = IronXL.Styles.HorizontalAlignment.Center;
@@ -61,12 +66,26 @@ namespace forms_librarie_app
 			wrapCellWithBorders(sheet, tableNameRange, BorderType.Thin);
 			sheet.Merge(tableNameRange);
 
-			var header = sheet["B3:I3"];
+			var header = sheet["B5:I5"];
 			header.Style.Font.Bold = true;
-			wrapCellWithBorders(sheet, "B3:I3", BorderType.Thin);
+			wrapCellWithBorders(sheet, "B5:I5", BorderType.Thin);
 			header.Style.ShrinkToFit = true;
 
 			return sheet;
+		}
+
+		private void writeDateToWorkSheet(WorkSheet sheet)
+		{
+			int startDataRow = 6;
+			int rowCounter = fetchFavoriteBooks().Length;
+			int offset = 2;
+			int dateRowNumber = rowCounter + startDataRow + offset;
+			char dateRowLetter = 'B';
+			string dateCellLocation = dateRowLetter.ToString() + dateRowNumber.ToString();
+
+			string date = DateTime.UtcNow.ToString("MM/dd/yyyy H:mm");
+			string dateCellContent = $"Data exportării: {date}";
+			sheet[dateCellLocation].Value = dateCellContent;
 		}
 
 		private string[] fetchFavoriteBooks()
@@ -115,7 +134,7 @@ namespace forms_librarie_app
 				return;
 			}
 				 
-			int firstRowNumber = 4;
+			int firstRowNumber = 6;
 			int rowNumber = firstRowNumber + rowIndex;
 			string[] columnLetters = new string[] {"B", "C", "D", "E", "F", "G", "H", "I"};
 
@@ -155,7 +174,15 @@ namespace forms_librarie_app
 				++rowCounter;
 			}
 
-			sheet.SaveAs("carti_favorite.xlsx");
+			writeDateToWorkSheet(sheet);
+			try
+			{
+				sheet.SaveAs("carti_favorite.xlsx");
+				MessageBox.Show("Datele au fost exportate!");
+			} catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
 		}
 	}
 }
